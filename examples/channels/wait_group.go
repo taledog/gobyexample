881e834b26a91ef53d6953952f40826abce84d39
arden <arden@tujiao.com>
2019-06-12 21:01:54 +0800

    update

--- examples/channels/wait_group.go
+++ examples/channels/wait_group.go
@@ -11,15 +11,14 @@ func main() {
 	for i := 0; i < 5; i = i + 1 {
 		wg.Add(1)
 		go func(n int) {
-			// defer wg.Done()
-			defer wg.Add(-1)
-			go EchoNumber(wg, n)
+			go EchoNumber(&wg, n)
 		}(i)
 	}
 	wg.Wait()
 }
 
-func EchoNumber(wg sync.WaitGroup, i int) {
+func EchoNumber(wg *sync.WaitGroup, i int) {
 	time.Sleep(3e9)
 	fmt.Println(i)
+	wg.Done()
 }
